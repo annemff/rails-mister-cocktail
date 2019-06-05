023c993c7f5aae0cdcab5c276a3242890b010f74
Rano Kuhl <ranokuhl@gmail.com>
2019-06-05 22:25:13 +0200

    Add db:seed w/ input from JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,62 +1,12 @@
-require 'open-uri'
 require 'json'
-
+require 'open-uri'
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 data = open(url).read
-drink = JSON.parse(data)
+drinks = JSON.parse(data)
 
-puts drink
-puts "#{drink['strIngredient1']}"
-
-
-
-# require "json"
-# require "rest-client"
-
-# response = RestClient.get "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-# repos = JSON.parse(response)
-# # => repos is an `Array` of `Hashes`.
-
-# repos.size
-
-
-
-
-# Ingredient.create!(data)
-
-# # This file should contain all the record creation needed to seed the database with its default values.
-# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-# #
-# # Examples:
-# #
-# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-# #   Character.create(name: 'Luke', movie: movies.first)
-
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
-# Ingredient.create(name: "wodka")
-# Ingredient.create(name: "rum")
-# Ingredient.create(name: "oranges")
-# Ingredient.create(name: "red bull")
-# Ingredient.create(name: "whisky")
-# Ingredient.create(name: "bacardi")
-# Ingredient.create(name: "malibu")
-# Ingredient.create(name: "coconut juice")
-# Ingredient.create(name: "water")
-# Ingredient.create(name: "gin")
-# Ingredient.create(name: "granat apple juice")
-# Ingredient.create(name: "rum")
-# Ingredient.create(name: "pina colada")
-# Ingredient.create(name: "bourbon")
-# Ingredient.create(name: "tequila")
-# Ingredient.create(name: "amaretto")
-# Ingredient.create(name: "triple sec")
-# Ingredient.create(name: "eggs")
-# Ingredient.create(name: "sugar")
-# Ingredient.create(name: "bitters")
-# Ingredient.create(name: "vermouth")
-# Ingredient.create(name: "sparkling wine")
-
-# puts "succe"
+drinks.each do |_drink, ingredients| # returns array of hashes
+  ingredients.each do |ingredient| # loop through the hash and put the value (ingredient) in the name: field of the Ingredient database
+    Ingredient.create(name: ingredient['strIngredient1'])
+  end
+end
